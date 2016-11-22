6627ff6ca8a97b46f270457277c91f4897dea0fe
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-11-22 11:20:44 +0100

    Initial Commit

---
+++ TennisKata/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TennisKata
+//
+//  Created by Tomás Ruiz López on 22/11/16.
+//  Copyright © 2016 Tomás Ruiz-López. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
