7fe44f45e2fd377e0afeb66e0fc5f4b088e9cc53
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-11-22 19:37:18 +0100

    Create extension to generate Point

---
+++ TennisKataTest/PointExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  PointExtensions.swift
+//  TennisKata
+//
+//  Created by Tomás Ruiz López on 22/11/16.
+//  Copyright © 2016 Tomás Ruiz-López. All rights reserved.
+//
+
+import Foundation
+import SwiftCheck
+@testable import TennisKata
+
+extension Point : Arbitrary {
+    public static var arbitrary : Gen<Point> {
+        return Gen<Point>.fromElementsOf([Point.love, Point.fifteen, Point.thirty])
+    }
+}
